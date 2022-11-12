af110d440973f78ad21d407da7deb05b5f0cb2d5
Олег Трифонов <iphone3460@yandex.ru>
2022-11-12 20:56:24 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -35,10 +35,10 @@ def defence(char_name: str, char_class: str) -> str:
 def special(char_name: str, char_class: str) -> str:
     """Описывает работу функции special."""
     if char_class == 'warrior':
-        return (f'{char_name} применил специальное умение'
+        return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
     if char_class == 'mage':
-        return (f'{char_name} применил специальное умение'
+        return (f'{char_name} применил специальное умение '
                 f'«Атака {5 + 40}»')
     if char_class == 'healer':
         return (f'{char_name} применил специальное умение '
@@ -79,16 +79,16 @@ def choice_char_class() -> str:
                                 'играть: Воитель — warrior, Маг — mage,'
                                 'Лекарь — healer: ')
         if char_class == 'warrior':
-            print('Воитель — дерзкий воин ближнего боя.'
+            print('Воитель — дерзкий воин ближнего боя. '
                   'Сильный, выносливый и отважный.')
         if char_class == 'mage':
-            print('Маг — находчивый воин дальнего боя.'
+            print('Маг — находчивый воин дальнего боя. '
                   'Обладает высоким интеллектом.')
         if char_class == 'healer':
-            print('Лекарь — могущественный заклинатель.'
+            print('Лекарь — могущественный заклинатель. '
                   'Черпает силы из природы, веры и духов.')
-        approve_choice: str = input('Нажми (Y), чтобы подтвердить выбор,'
-                                    'или любую другую кнопку, чтобы'
+        approve_choice: str = input('Нажми (Y), чтобы подтвердить выбор, '
+                                    'или любую другую кнопку, чтобы '
                                     'выбрать другого персонажа ').lower()
     return char_class
 
